ceda10b68f971edbf7c4d4626b0639723ecccc31
mateuszchmura94-dev <mateuszchmura94@gmail.com>
2026-05-06 20:05:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var pressed = false;
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+})
+
+  $(document).keypress(function () {
+    if (pressed === false){
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      pressed = true;
+    }
+  });
+
+    function nextSequence() {
+      userClickedPattern = [];
+      var randomNumber = Math.round(Math.random() * 3);
+      var randomChosenColour = buttonColours[randomNumber];
+      gamePattern.push(randomChosenColour);
+      $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+      playSound(randomChosenColour);
+      level++;
+      $("#level-title").text("Level " + level);
+
+    }
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  pressed = false;
+}
